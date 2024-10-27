874de4fbd974581d8409bf0a52e831c4191fd1d8
dinil-thilakarathne <sonacode44@gmail.com>
2024-10-27 21:07:53 +0530

    feat: add theme toggle

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,3 +1,21 @@
+import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "@/components/ThemeToggle";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <main>{children}</main>;
+  return (
+    <main>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <div className="fixed right-4 top-4 z-[99999]">
+          <ModeToggle />
+        </div>
+        {children}
+      </ThemeProvider>
+      {children}
+    </main>
+  );
 }
