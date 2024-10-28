79771e41af22163ccd0f042bc8755375c02d143a
dinil-thilakarathne <sonacode44@gmail.com>
2024-10-29 02:18:48 +0530

    Update Components query

--- src/sanity/lib/queries.ts
+++ src/sanity/lib/queries.ts
@@ -11,3 +11,13 @@ export const TESTIMONIALS_QUERY = defineQuery(`*[_type == "testimonial"]{
   },
   description
 }`);
+
+export const SONAUI_COMPONENTS_QUERY =
+  defineQuery(`*[_type == "sonauiComponent"] | order(name asc){
+    _id,
+    name,
+    pageLink,
+    status,
+    category,
+    tags
+  }`);
